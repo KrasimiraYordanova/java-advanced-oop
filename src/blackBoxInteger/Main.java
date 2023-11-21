b148207b97391032f79897806f239679b3587bd7
krasimiranova <krasy_nova@yahoo.com>
2023-11-21 17:15:31 +0100

    reflection & annotation ex 02

--- src/blackBoxInteger/Main.java
+++ src/blackBoxInteger/Main.java
@@ -1,7 +1,36 @@
 package blackBoxInteger;
 
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Class<BlackBoxInt> classs = BlackBoxInt.class;
+        Constructor<BlackBoxInt> constructor = classs.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        BlackBoxInt blackBoxInt = constructor.newInstance();
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        Field hiddenValue = classs.getDeclaredField("innerValue");
+        hiddenValue.setAccessible(true);
+
+
+        while(!"END".equals(command)) {
+            String[] tokens = command.split("_");
+            String methodName = tokens[0];
+            int parameter = Integer.parseInt(tokens[1]);
+
+            Method method = classs.getDeclaredMethod(methodName, int.class);
+            method.setAccessible(true);
+            method.invoke(blackBoxInt, parameter);
+
+            System.out.println(hiddenValue.get(blackBoxInt));
+            command = scanner.nextLine();
+        }
     }
 }
