9f732609636ec9a7decf722c8f37f5ec12dc7a99
krasimiranova <krasy_nova@yahoo.com>
2023-11-23 11:24:15 +0100

    exceptions and error handling lab 02

---
+++ src/SquareRoot/Main.java
@@ -0,0 +1,25 @@
+package SquareRoot;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(input);
+            if(number >= 0) {
+                double sqrt = Math.sqrt(number);
+                System.out.printf("%.2f%n", sqrt);
+            }
+        } catch(NumberFormatException ignored) {
+            System.out.println("Invalid");
+        } finally {
+            System.out.println("Goodbye");
+        }
+
+    }
+}
